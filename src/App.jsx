c95e1d1b8e36d0e1f23f76c8eb25b80e1aa21ca8
usama_forayaje <usamaforayaje03@gmail.com>
2024-12-21 22:06:17 +0600

    final

--- src/App.jsx
+++ src/App.jsx
@@ -57,20 +57,7 @@ function App() {
               className="flex items-center justify-center text-white transition transform rounded-full shadow-lg w-14 h-14 bg-gradient-to-r from-blue-500 to-blue-700 hover:scale-110"
               onClick={swap}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-8 h-8"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 4v5h.582M20 20v-5h-.582M7 11l-3 3m0 0l3 3m-3-3h16M17 13l3-3m0 0l-3-3m3 3H4"
-                />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" ><path d="M17 21v-2a1 1 0 0 1-1-1v-1a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v1a1 1 0 0 1-1 1"/><path d="M19 15V6.5a1 1 0 0 0-7 0v11a1 1 0 0 1-7 0V9"/><path d="M21 21v-2h-4"/><path d="M3 5h4V3"/><path d="M7 5a1 1 0 0 1 1 1v1a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a1 1 0 0 1 1-1V3"/></svg>
             </button>
           </div>
           <InputBox
